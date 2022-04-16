21b1e03a2e756aeae4b619742d122fe7cd7587af
ShantanuKudva <98833827+ShantanuKudva@users.noreply.github.com>
2022-04-16 19:06:56 +0530

    added winning stage

--- script.js
+++ script.js
@@ -16,75 +16,80 @@ let score1 = 0;
 
 //dice roll functionality
 const diceclick = function () {
-  random = Math.floor(Math.random() * 6) + 1;
-  console.log(random);
-  if (player0.classList.contains('player--active')) {
-    if (random === 1) {
-      dice.src = 'dice-1.png';
-      score0 = 0;
-      currentScore0.textContent = 0;
-      player0.classList.remove('player--active');
-      player1.classList.add('player--active');
-      player0Score.textContent = 0;
-    } else if (random === 2) {
-      dice.src = 'dice-2.png';
-      currentScore0.textContent = 2;
-      score0 += random;
-      player0Score.textContent = score0;
-    } else if (random === 3) {
-      dice.src = 'dice-3.png';
-      currentScore0.textContent = 3;
-      score0 += random;
-      player0Score.textContent = score0;
-    } else if (random === 4) {
-      dice.src = 'dice-4.png';
-      currentScore0.textContent = 4;
-      score0 += random;
-      player0Score.textContent = score0;
-    } else if (random === 5) {
-      dice.src = 'dice-5.png';
-      currentScore0.textContent = 5;
-      score0 += random;
-      player0Score.textContent = score0;
-    } else if (random === 6) {
-      dice.src = 'dice-6.png';
-      currentScore0.textContent = 6;
-      score0 += random;
-      player0Score.textContent = score0;
-    }
-  } else if (player1.classList.contains('player--active')) {
-    if (random === 1) {
-      dice.src = 'dice-1.png';
-      currentScore1.textContent = 0;
-      player1.classList.remove('player--active');
-      player0.classList.add('player--active');
-      player1Score.textContent = 0;
-      score1 = 0;
-    } else if (random === 2) {
-      dice.src = 'dice-2.png';
-      currentScore1.textContent = 2;
-      score1 += random;
-      player1Score.textContent = score1;
-    } else if (random === 3) {
-      dice.src = 'dice-3.png';
-      currentScore1.textContent = 3;
-      score1 += random;
-      player1Score.textContent = score1;
-    } else if (random === 4) {
-      dice.src = 'dice-4.png';
-      currentScore1.textContent = 4;
-      score1 += random;
-      player1Score.textContent = score1;
-    } else if (random === 5) {
-      dice.src = 'dice-5.png';
-      currentScore1.textContent = 5;
-      score1 += random;
-      player1Score.textContent = score1;
-    } else if (random === 6) {
-      dice.src = 'dice-6.png';
-      currentScore1.textContent = 6;
-      score1 += random;
-      player1Score.textContent = score1;
+  if ((player0Score.textContent === 100) | (player1Score.textContent === 100)) {
+    roll.textContent = '⚠ GAME OVER ⚠!';
+    roll.addEventListener('click', resetClick);
+  } else if (player0Score.textContent < 100 && player1Score.textContent < 100) {
+    random = Math.floor(Math.random() * 6) + 1;
+    console.log(random);
+    if (player0.classList.contains('player--active')) {
+      if (random === 1) {
+        dice.src = 'dice-1.png';
+        score0 = 0;
+        currentScore0.textContent = 0;
+        player0.classList.remove('player--active');
+        player1.classList.add('player--active');
+        player0Score.textContent = 0;
+      } else if (random === 2) {
+        dice.src = 'dice-2.png';
+        currentScore0.textContent = 2;
+        score0 += random;
+        player0Score.textContent = score0;
+      } else if (random === 3) {
+        dice.src = 'dice-3.png';
+        currentScore0.textContent = 3;
+        score0 += random;
+        player0Score.textContent = score0;
+      } else if (random === 4) {
+        dice.src = 'dice-4.png';
+        currentScore0.textContent = 4;
+        score0 += random;
+        player0Score.textContent = score0;
+      } else if (random === 5) {
+        dice.src = 'dice-5.png';
+        currentScore0.textContent = 5;
+        score0 += random;
+        player0Score.textContent = score0;
+      } else if (random === 6) {
+        dice.src = 'dice-6.png';
+        currentScore0.textContent = 6;
+        score0 += random;
+        player0Score.textContent = score0;
+      }
+    } else if (player1.classList.contains('player--active')) {
+      if (random === 1) {
+        dice.src = 'dice-1.png';
+        currentScore1.textContent = 0;
+        player1.classList.remove('player--active');
+        player0.classList.add('player--active');
+        player1Score.textContent = 0;
+        score1 = 0;
+      } else if (random === 2) {
+        dice.src = 'dice-2.png';
+        currentScore1.textContent = 2;
+        score1 += random;
+        player1Score.textContent = score1;
+      } else if (random === 3) {
+        dice.src = 'dice-3.png';
+        currentScore1.textContent = 3;
+        score1 += random;
+        player1Score.textContent = score1;
+      } else if (random === 4) {
+        dice.src = 'dice-4.png';
+        currentScore1.textContent = 4;
+        score1 += random;
+        player1Score.textContent = score1;
+      } else if (random === 5) {
+        dice.src = 'dice-5.png';
+        currentScore1.textContent = 5;
+        score1 += random;
+        player1Score.textContent = score1;
+      } else if (random === 6) {
+        dice.src = 'dice-6.png';
+        currentScore1.textContent = 6;
+        score1 += random;
+        player1Score.textContent = score1;
+      }
     }
   }
 };
@@ -112,6 +117,9 @@ const resetClick = function () {
       0;
 };
 
+//event listeners
 roll.addEventListener('click', diceclick);
 hold.addEventListener('click', holdclick);
 refresh.addEventListener('click', resetClick);
+
+console.log(player0Score.textContent, typeof player0Score.textContent);
